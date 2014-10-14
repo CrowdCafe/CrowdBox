531325014127c2c6304e0ac222d93ca242adb02a
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2014-10-14 17:14:41 +0200

    no message

--- webhook/urls.py
+++ webhook/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 import views
 
 urlpatterns = patterns('',
-	url(r'webhook_dropbox/$', views.webhook_dropbox, name= 'imagesource-webhook'),
-	#url(r'webhook_crowdcafe/$', views.webhook_crowdcafe, name= 'imagesource-crowdcafe'),
+	url(r'dropbox/$', views.webhook_dropbox, name= 'imagesource-webhook'),
+	#url(r'crowdcafe/$', views.webhook_crowdcafe, name= 'imagesource-crowdcafe'),
 	url(r'getMediaLink/(?P<uid>\d+)/$', views.getMediaLink, name= 'imagesource-get-image-link'),
 )
