a31acdcc3735959cb3def7af5ce54dc226925a58
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2014-10-24 16:10:44 +0200

    no message

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-    (r'^$', TemplateView.as_view(template_name='welcome.html')),
-    (r'^info/$', TemplateView.as_view(template_name='info.html')),
+    (r'^$', TemplateView.as_view(template_name='welcome.html', name='welcome')),
+    (r'^info/$', TemplateView.as_view(template_name='info.html', name='info')),
 )
