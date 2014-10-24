2c15bfd2525ef3c21a18f3523615d75eb178db67
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2014-10-24 16:14:59 +0200

    no message

--- website/urls.py
+++ website/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns,url
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-    url(r'^mine/$',TemplateView.as_view(template_name='welcome.html'), name='website-welcome'),
-    url(r'^mine/$',TemplateView.as_view(template_name='info.html'), name='website-info'),
+    url(r'^$',TemplateView.as_view(template_name='welcome.html'), name='website-welcome'),
+    url(r'^info/$',TemplateView.as_view(template_name='info.html'), name='website-info'),
 
 )
